1671f5b167fad17a44cc3ec5a1c764bd88dcc553
Mezzky <ryansahadha@gmail.com>
2024-01-11 20:23:02 +0800

    Add Link

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -5,6 +5,12 @@ const home = () => {
   return (
     <div>
       <h1 className='font-bold text-blue-400 text-5xl'>Welcome to my Website</h1>
+      <nav>
+        <ul>
+          <li><a href="./login">Login Page</a></li>
+          <li><a href="./register">Register Page</a></li>
+        </ul>
+      </nav>
     </div>
   )
 }
